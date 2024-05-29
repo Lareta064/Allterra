40ed02bf08e14af6532f921bd10dc66674faaf81
Lareta064 <lareta064@gmail.com>
2024-05-29 17:49:20 +0300

    search-script

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", function (){
-	
+	/*==============FOR HEADER SEARCH FORM ============= */
+	const bodyEl = document.body;
+	const openSearchForm = document.querySelector('#search-btn');
+	const searchFormPopup = document.querySelector('#search-popup');
+	if(openSearchForm){
+		openSearchForm.addEventListener('click', ()=>{
+			bodyEl.classList.add('lock');
+			if(searchFormPopup.classList.contains('active')){
+				searchFormPopup.classList.remove('active');
+			}else{
+				searchFormPopup.classList.add('active');
+			}
+			
+		})
+	}
 
 });
