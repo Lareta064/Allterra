d3e6e8b5389ca132d97a64c490324ebdfb7f9b33
Lareta064 <lareta064@gmail.com>
2024-05-29 19:12:36 +0300

    mob-search-form

--- src/js/main.js
+++ src/js/main.js
@@ -15,4 +15,34 @@ document.addEventListener("DOMContentLoaded", function (){
 		})
 	}
 
+	const menuToggle = document.querySelector('#menu-toggle');
+	const mobileMenu = document.querySelector('#header-menu');
+	
+
+	if (menuToggle) {
+
+		/*   клик поиконке гамбургер*/  
+		menuToggle.addEventListener('click', ()=> {
+			
+			if (menuToggle.classList.contains('active')) {
+
+				menuToggle.classList.remove('active');
+				mobileMenu.classList.remove('active');
+				bodyEl.classList.remove('lock');
+			
+			} else {
+				menuToggle.classList.add('active');
+			    mobileMenu.classList.add('active');
+				bodyEl.classList.add('lock');
+			}
+		});
+
+       /*   клик по мобильному меню*/  
+		mobileMenu.addEventListener('click', () => {
+			menuToggle.classList.remove('active');
+			mobileMenu.classList.remove('active');
+			bodyEl.classList.remove('lock');
+		});
+	}
+
 });
